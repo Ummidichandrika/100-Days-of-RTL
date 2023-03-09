dfc8f625f4f62e7ae7d6ac242a4c39c9efb091e0
Ummidichandrika <92587990+Ummidichandrika@users.noreply.github.com>
2023-03-09 23:06:54 +0530

    Create tb_automatic_variables.sv

---
+++ TB To Verify Automatic Variables [Day 76]/tb_automatic_variables.sv
@@ -0,0 +1,27 @@
+module automatic_var();
+  
+  initial begin
+    fork
+      begin
+        for (int i=0; i<8; i++) begin
+          // Copy the loop variable in an automatic variable
+          automatic int j = i;
+          fork
+            begin
+              print(j);
+            end
+          join_none
+        end
+        wait fork;
+      end
+    join
+    $display("****After wait work*****");
+  end
+      
+  task automatic print(int i);
+    automatic int rand_delay;
+    rand_delay = $urandom_range(1, 10);
+    #rand_delay;
+    $display("%t Thread[%0d] finished", $time, i);
+  endtask
+  endmodule

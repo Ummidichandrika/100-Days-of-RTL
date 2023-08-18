781123e4058319aa4c2bf9b0924e4ab0342314c7
Ummidichandrika <92587990+Ummidichandrika@users.noreply.github.com>
2023-08-18 14:21:21 +0530

    Update clockdivider.v

--- Clock Divider [Day 1]/clockdivider.v
+++ Clock Divider [Day 1]/clockdivider.v
@@ -3,16 +3,16 @@
 module clockdivider(clk,divideby2,divideby4,divideby8,rst);
 input clk,rst;
 reg [3:0]count;
-output divideby2,divideby4,divideby8;
-reg divideby2,divideby4,divideby8;
+output reg divideby2,divideby4,divideby8,divideby16;
 always@(posedge clk)
 begin
 if(rst==0)
 count=4'b0000;
 else
 count=count+1;
-divideby2=count[1];
-divideby4=count[2];
-divideby8=count[3];
+  divideby2=count[0];
+  divideby4=count[1];
+  divideby8=count[2];
+  divideby16=count[3];
 end
 endmodule
